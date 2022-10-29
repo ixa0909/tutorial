445710d6f9d55f1caf31d79ce842bc1db41453ce
kazukiObo <obozaurusu@gmail.com>
2022-10-30 05:15:29 +0900

    カレンダーに値渡せた

--- src/js/go-react.jsx
+++ src/js/go-react.jsx
@@ -9,16 +9,12 @@ import InputComponent from "./InputComponent"
 import FilterComponent from "./FilterComponent"
 import ListComponent from "./ListComponent"
 
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import jaLocale from '@fullcalendar/core/locales/ja';
-
 
 import Button from '@mui/material/Button';
 import * as MUI from '@mui/material/';
 import AddIcon from '@mui/icons-material/Add';
 
+import Calender from "./Calender";
 
 function Push() {
   return <Button variant="contained">Hello World</Button>;
@@ -40,7 +36,7 @@ function Layout() {
     })()
   }, [])
 
-
+  const plan = "gargaga"
   const add = (t) => {
     setTodoItems([...todoItems, { id: todoItems.length + 1, t: t, c: false }])
   }
@@ -70,22 +66,8 @@ function Layout() {
         <AddIcon />
       </MUI.Fab>
       
+      <Calender plan={plan}></Calender>
       
-      <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin]}
-        initialView="dayGridMonth"
-        locales={[jaLocale]}
-        locale='ja'
-        headerToolbar={{
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek',
-        }}
-        events={[
-          {title:'eventを', start: '2022-03-14'},
-          {title:'こんな感じで追加できます', start: '2022-03-15', end: '2022-03-17'}
-        ]}
-      />
       <InputComponent add={add} />
       <FilterComponent />
       <ListComponent todoItems={todoItems} upd={upd} remove={remove} />
