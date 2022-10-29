2ecd0ec8e2ba0da730721919adbc21ec25d785ca
kazukiObo <obozaurusu@gmail.com>
2022-10-30 02:53:23 +0900

    セーブcssのコンパイルから再開

--- src/js/ListComponent.js
+++ src/js/ListComponent.js
@@ -6,9 +6,23 @@ export default function ListComponent(props) {
             <ul>
                 {props.todoItems.map(todoItem =>
                 <li>
-                    <span>タイトル</span>
-                    <span><input type="checkbox"/></span>
-                    <span><button>削除</button></span> 
+                    <span id ="title">{todoItem.t}</span>
+                    <span>
+                        <input type="checkbox"
+                        checked={todoItem.c}
+                        onChange={e=>{
+                            props.upd(todoItem.id)
+                        }}
+                        />
+                    </span>
+                    <span>
+                        <button onClick={e=>{
+                            e.preventDefault()
+                            props.remove(todoItem.id)
+                        }}>
+                            削除
+                        </button>
+                    </span> 
                 </li>
                 )}    
             </ul>
--- src/js/go-react.jsx
+++ src/js/go-react.jsx
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom";
 import Mori from "./Mori"
 // import ToDoApp from "./ToDoApp"
 
+// import "../css/styleConverted.css"
+
 import InputComponent from "./InputComponent"
 import FilterComponent from "./FilterComponent"
 import ListComponent from "./ListComponent"
-
-
-
 
 function Layout (){
   // 初期化はいらない
@@ -17,11 +16,11 @@ function Layout (){
   useEffect(() =>{
     (async () =>{
       setTodoItems([
-        {id:1,t:"1",c:true},
-        {id:1,t:"1",c:true},
-        {id:1,t:"1",c:true},
-        {id:1,t:"1",c:true},
-        {id:1,t:"1",c:true},
+        {id:1,t:"眠る",c:true},
+        {id:2,t:"食べる",c:true},
+        {id:3,t:"運動する",c:true},
+        {id:4,t:"研究する",c:true},
+        {id:5,t:"遊ぶ",c:true},
       ])
     })()
   },[])
@@ -31,12 +30,25 @@ function Layout (){
     setTodoItems([...todoItems,{id:todoItems.length+1,t:t,c:false}])
   }
 
+  const upd=(id)=>{
+    setTodoItems(todoItems.map(todoItem => {
+      if (todoItem.id === id){
+        todoItem.c = !todoItem.c
+      }
+      return todoItem
+    }))
+  }
+
+  const remove = (id)=>{
+    setTodoItems(todoItems.filter(todoItem =>todoItem.id !== id))
+  }
+
   
     return (
       <div>
         <InputComponent add={add}/>
         <FilterComponent/>
-        <ListComponent todoItems={todoItems}/>
+        <ListComponent todoItems={todoItems} upd={upd} remove={remove} />
       </div>
     );
   }
