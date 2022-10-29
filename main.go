ba6a5e67b8eabe8ac26f2ab38eda8a253d561a1b
kazukiObo <obozaurusu@gmail.com>
2022-10-30 06:37:17 +0900

    テンプレhtml持ってきた

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func main() {
 	router.Static("/src/css", "./src/css")
 	router.Static("/src/js", "./src/js")
 
+
+	router.Static("/images", "./images")
+	router.Static("/images_photo", "./images_photo")
+
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.HTML(200, "index.html", gin.H{
 			"a": "変数をバックエンドからフロントへ渡しています",
--- src/js/InputComponent.jsx
+++ src/js/InputComponent.jsx
@@ -1,26 +1,34 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 import TextField from '@mui/material/TextField';
+import Switch from '@mui/material/Switch';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 
 export default function InputComponent(props) {
 
-    const [t,setT] = useState('')
+    const [t, setT] = useState('')
 
-    const changeT =(e)=>{
+    const changeT = (e) => {
         setT(e.target.value)
     }
-    const save =(e)=>{
+    const save = (e) => {
         e.preventDefault()
         props.add(t)
         setT('')
     }
     return (
         <form>
-            <TextField id="outlined-search" 
-                label="to do を入力" value={t} 
-                    type="search" onChange={changeT}
-                        color="secondary"/>
-            <button onClick={save}>保存</button>
+            <TextField id="outlined-search"
+                label="to do を入力" value={t}
+                type="search" onChange={changeT}
+                color="secondary" size="medium" />
+
+            <Button variant="contained" endIcon={<SendIcon />} onClick={save} size="large">
+                登録
+            </Button>
+
+
         </form>
     )
 }
