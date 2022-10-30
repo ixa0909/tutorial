f2ec13efd5a176460355cac74d95c4b9482704a7
kazukiObo <obozaurusu@gmail.com>
2022-10-30 14:52:54 +0900

    [remove]キャッシュを消して再度push

--- gitPush.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Mac
-cp  /Users/kazuki_yabuuchi/Documents/GitHub/tutorial/src/js/go-react.min.js /Users/kazuki_yabuuchi/Documents/GitHub/tutorial/docs/
-mv /Users/kazuki_yabuuchi/Documents/GitHub/tutorial/docs/go-react.min.js /Users/kazuki_yabuuchi/Documents/GitHub/tutorial/docs/index.js
-read -p "Please input your comment:  " comment
-git add -A 
-git commit -a -m $comment
-git push
