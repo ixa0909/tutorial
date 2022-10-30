cdeafbf74d5431206afdc4661eeaee6163ec8e83
kazukiObo <obozaurusu@gmail.com>
2022-10-30 16:04:58 +0900

    背景画像のサイズを自動調整

--- main.go
+++ main.go
@@ -10,21 +10,19 @@ func main() {
 	router.Static("/src/css", "./src/css")
 	router.Static("/src/js", "./src/js")
 
-
-	router.Static("/images", "./images")
-	router.Static("/images_photo", "./images_photo")
+	router.Static("/src/images", "./src/images")
 
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(200, "index.html", gin.H{
+		ctx.HTML(200, "home.html", gin.H{
 			// "a": "変数をバックエンドからフロントへ渡しています",
 		})
 	})
 
-	// router.GET("/side", func(ctx *gin.Context) {
-	// 	ctx.HTML(200, "side.html", gin.H{
-	// 		"b": "aga",
-	// 	})
-	// })
+	router.GET("/side", func(ctx *gin.Context) {
+		ctx.HTML(200, "side.html", gin.H{
+			"b": "aga",
+		})
+	})
 
 	
 
